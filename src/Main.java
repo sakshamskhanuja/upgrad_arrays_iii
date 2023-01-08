72d23a59772ff226805896ce137504c59a507910
Saksham Singh Khanuja <sskhanuja94@gmail.com>
2023-01-09 01:20:25 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // Stores names.
+        String[] names = new String[4];
+
+        // Stores "Henry" and "Shane".
+        names[0] = "Henry";
+        names[1] = "Shane";
+
+        // Stores "Sharma" and "Watson".
+        names[2] = "Sharma";
+        names[3] = "Watson";
+
+        // Prints the full name.
+        System.out.println(names[0] + " " + names[2]);
+        System.out.println(names[1] + " " + names[3]);
+    }
+}
